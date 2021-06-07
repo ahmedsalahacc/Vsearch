2d54eef32ac88599d16bcdbcfb10fb529abb271a
Ahmed Salah A. Maksoud <Ahmedsalaheldin125@gmail.com>
2021-06-08 01:16:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def handle_bad_request(e):
     return 'bad request!', 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
